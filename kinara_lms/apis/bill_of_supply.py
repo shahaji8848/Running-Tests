d8d727a07a60f88c14d106ff0281aceb52dd03a0
tohidmalik002 <touhid@8848digital.com>
2023-12-29 11:53:19 +0530

    chore: bill of supplay and loan closure get_pdf api update

--- kinara_lms/apis/bill_of_supply.py
+++ kinara_lms/apis/bill_of_supply.py
@@ -11,4 +11,4 @@ def get_bill_of_supply_pdf(**kwargs):
     doc.save()
     base_url = frappe.utils.get_url()
     pdf_url = f"{base_url}/api/method/frappe.utils.print_format.download_pdf?doctype=Bill%20of%20Supply&name={doc.name}&format=Bill%20of%20Supply&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=en"
-    return{"pdf_url": pdf_url}
+    return{"pdf": pdf_url}
