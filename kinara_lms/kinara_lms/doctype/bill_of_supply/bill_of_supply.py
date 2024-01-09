6884780a8a81abd5cdafc2b23034f11ee91e8e33
Touhid Mullick <touhid@8848digital.com>
2024-01-09 22:17:39 +0530

    bill ofix: ffsuppby  api ucalculation bug fix

--- kinara_lms/kinara_lms/doctype/bill_of_supply/bill_of_supply.py
+++ kinara_lms/kinara_lms/doctype/bill_of_supply/bill_of_supply.py
@@ -5,10 +5,12 @@
 import frappe
 import datetime
 from frappe.model.document import Document
+from frappe.utils.data import flt
+
 
 class BillofSupply(Document):
     def before_save(self):
         total = 0
         for item in self.items:
-            total += item.amount
+            total += flt(item.amount)
         self.append('items',{'particulars':"Total Amount",'amount':total})
