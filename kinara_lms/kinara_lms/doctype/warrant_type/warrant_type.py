f365a9e03cd949b11e95d7544e240f12f30fd680
shahaji8848 <shahaji@8848digital.com>
2024-01-19 18:44:41 +0530

    test06

--- kinara_lms/kinara_lms/doctype/warrant_type/warrant_type.py
+++ kinara_lms/kinara_lms/doctype/warrant_type/warrant_type.py
@@ -7,3 +7,25 @@ from frappe.model.document import Document
 
 class WarrantType(Document):
 	pass
+      kinara_lms/kinara_lms/doctype/warrant_type/warrant_type.py
+# Copyright (c) 2023, Visage Holdings and Finance Private Limited) and contribu>
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WarrantType(Document):
+        pass
+
+      kinara_lms/kinara_lms/doctype/warrant_type/warrant_type.py
+# Copyright (c) 2023, Visage Holdings and Finance Private Limited) and contribu>
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WarrantType(Document):
+        pass
+
